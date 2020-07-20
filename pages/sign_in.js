332cc6e8264e0a99d62b10654d184fa52e55c12c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-20 21:27:24 +0100

    Create sign_in.js

---
+++ pages/sign_in.js
@@ -0,0 +1,38 @@
+$.pages["Sign In"] = (() => {
+
+const load = ($d, $cb) => {
+	$cb(main);
+};
+
+const main = document.createElement("main");
+main.ngstyle = {"display":"grid", "place-items":"center"};
+
+let form = document.createElement("form"); main.appendChild(form);
+	
+let div = document.createElement("div"); form.appendChild(div);
+div.ngstyle = {"display":"grid", "background-color":"rgb(255,255,255,0.67)", "padding":"20px", "border-radius":"10px", "grid-template-columns":"auto auto", "grid-gap":"5px"};
+
+let label = document.createElement("label"); div.appendChild(label); label.textContent = "Username";
+let input = document.createElement("input"); div.appendChild(input);
+input.type = "text";
+input.required = true;
+input.autocomplete = "username";
+	
+label = document.createElement("label"); div.appendChild(label); label.textContent = "Password";
+input = document.createElement("input"); div.appendChild(input);
+input.type = "password";
+input.required = true;
+input.autocomplete = "current-password";
+
+let button = document.createElement("button"); div.appendChild(button);
+button.ngstyle = {"grid-column":"1/-1"};
+button.className = "faS";
+button.textContent = " Sign In";
+button.ngpointerdown = () => {
+	if (form.reportValidity() === false) return;
+	$.nav.goTo("Staff List");
+};
+
+return load;
+
+})();
