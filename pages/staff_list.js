dbead5b78bce0cd2fba3a5900ad0cb77fdb21fdb
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-20 22:25:54 +0100

    Update staff_list.js

--- pages/staff_list.js
+++ pages/staff_list.js
@@ -8,10 +8,28 @@ const load = ($in, $cb) => {
 };
 
 const main = document.createElement("main");
-main.textContent = "132";
 
-const build = () => {};
+const build = ($d) => {
+
+	main.textContent = "";
 	
+	let department = null;
+	$d[1].forEach($v => {
+		
+		if (department !== $v.Department) {
+			let fieldset = document.createElement("fieldset"); main.appendChild(fieldset);
+			let legend = document.createElement("legend"); fieldset.appendChild(legend);
+			legend.textContent = $v.Department;
+			let table = document.createElement("table"); fieldset.appendChild(table);
+		};
+		
+		let tr = document.createElement("tr"); table.appendChild(tr);
+		let td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.Name;
+		
+	});
+
+};
+
 return load;
 
 })();
