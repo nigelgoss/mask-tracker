7df54d3c327ca5aefe2342928f5f46821c70ed5c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-20 20:31:47 +0100

    Create db.js

---
+++ db.js
@@ -0,0 +1,32 @@
+"use strict";
+$.db = (() => {
+	
+const query = ($qry, $params = {}, $success) => {
+
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = async () => {
+		if (xhr.readyState !== 4) return;
+		if (xhr.status === 200) $success(JSON.parse(xhr.responseText));
+		spinner(false);
+	};
+	xhr.open("GET", "!/"+$qry+".sql", true);
+	xhr.send(JSON.stringify([$qry, $params]));
+	spinner(true);
+	
+};
+
+const spinner = ($status) => {
+	if ($status === true) { document.body.appendChild(spinnerDiv); } else { spinnerDiv.remove(); };
+};
+
+const spinnerDiv = document.createElement("div");
+spinnerDiv.ngstyle = {"position":"absolute", "top":"0", "left":"0", "width":"100%", "height":"100%", "place-items":"center", "display":"grid"};
+let img = document.createElement("img"); spinnerDiv.appendChild(img);
+img.src = "resources/spinner.png";
+img.ngstyle = {"animation":"transformRotate 2s infinite linear"};
+
+return {
+	query: query,
+};
+  
+})();
