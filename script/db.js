94aa0ba4330ca614974234630911f85e2190be1c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-20 21:57:21 +0100

    Update db.js

--- script/db.js
+++ script/db.js
@@ -1,14 +1,30 @@
 $.db = (() => {
+
+let session = null;
 	
 const query = ($qry, $params = {}, $success) => {
 
+	if (session === null) {
+		let u = document.querySelector("#username").value;
+		let p = document.querySelector("#password").value;
+		session = JSON.stringify([u, p]);
+		u = "";
+		p = "";
+	};
+	
 	const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = async () => {
 		if (xhr.readyState !== 4) return;
-		if (xhr.status === 200) $success(JSON.parse(xhr.responseText));
+		if (xhr.status === 200) {
+			$success(JSON.parse(xhr.responseText));
+		} else if (xhr.status === 401) {
+			session = null;
+			$.nav.goTo("Sign In");
+		};
 		spinner(false);
 	};
 	xhr.open("GET", "!/"+$qry+".sql", true);
+	xhr.setRequestHeader("auth", session);
 	xhr.send(JSON.stringify([$qry, $params]));
 	spinner(true);
 	
